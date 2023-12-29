618e712e192309fc93a6f5b327904b681ca301cc
EATGao <gaoyurong923@gmail.com>
2023-12-29 16:04:40 +1100

    feat: login page without backend finished

--- src/views/Login/Login.tsx
+++ src/views/Login/Login.tsx
@@ -1,6 +1,6 @@
 import styles from './login.module.scss'
 import initLoginBackground from './init.ts'
-import { useEffect } from 'react'
+import { ChangeEvent, useEffect, useState } from 'react'
 import { Button, Input, Space } from 'antd'
 import './login.less'
 
@@ -9,6 +9,26 @@ const Login = () => {
 		initLoginBackground();
 		window.onresize = function(){initLoginBackground()}
 	}, [])
+
+	const [userName, setUserName] = useState("");
+	const [password, setPassword] = useState("");
+	const [captcha, setCaptcha] = useState("");
+
+	const userNameChange = (e: ChangeEvent<HTMLInputElement>) => {
+		setUserName(e.target.value)
+	}
+
+	const passwordChange = (e: ChangeEvent<HTMLInputElement>) => {
+		setPassword(e.target.value)
+	}
+
+	const captchaChange = (e: ChangeEvent<HTMLInputElement>) => {
+		setCaptcha(e.target.value)
+	}
+
+	const login = () => {
+		console.log(userName + ' ' + password + ' ' + captcha)
+	}
 
 	return (
 		<div className={styles.loginPage}>
@@ -20,16 +40,16 @@ const Login = () => {
           </div>
 					<div className='form'>
 						<Space direction='vertical' size='large' style={{ display: 'flex' }}>
-							<Input placeholder="User Name" />
-							<Input.Password placeholder='Password' />
+							<Input placeholder="User Name" onChange={userNameChange}/>
+							<Input.Password placeholder='Password' onChange={passwordChange}/>
 							<div className="captchaBox">
-								<Input placeholder='Verification Code' />
+								<Input placeholder='Verification Code' onChange={captchaChange} />
 								<div className='captchaImg'>
 									{/* add verification code later*/}
 									<img height="38"  src="" alt="" />
 								</div>
 							</div>
-							<Button className='loginButton' type='primary' block>
+							<Button className='loginButton' type='primary' block onClick={login}>
 								Login
 							</Button>
 						</Space>
