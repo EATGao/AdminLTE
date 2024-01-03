76ef6c88c03bf7a35cde7fb95ec23f23c1cffb32
EATGao <gaoyurong923@gmail.com>
2024-01-03 11:42:50 +1100

    feat: add route guard

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,52 @@
-import { useRoutes } from "react-router-dom"
+import { useRoutes, useLocation, useNavigate } from "react-router-dom"
 import router from "./router"
+import { useEffect } from "react"
+import { message } from "antd"
+
+function ToPage1() {
+  const navigateTo = useNavigate()
+  useEffect(() => {
+    navigateTo("/page1")
+  }, [])
+
+  return <div></div>
+}
+
+function ToLogin() {
+  const navigateTo = useNavigate()
+  useEffect(() => {
+    navigateTo("/login")
+    message.warning("Please Login first")
+  }, [])
+
+  return <div></div>
+}
+
+function BeforeRouterEnter() {
+  const outlet = useRoutes(router)
+  const location = useLocation()
+
+  // to Login page and with token
+
+  // to other pages and no token
+
+  let token = localStorage.getItem("adminlte-token")
+
+  if (location.pathname === "/login" && token) {
+
+    return <ToPage1 />
+  } else if (location.pathname !== "/login" && !token) {
+
+    return <ToLogin />
+  }
+
+  return outlet
+}
 
 function App() {
-
-  const outlet = useRoutes(router)
   return (
     <div>
-      {outlet}
+      <BeforeRouterEnter />
     </div>
   )
 }
